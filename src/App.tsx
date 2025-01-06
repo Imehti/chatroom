5117d74f3083e0cb625d0e47dd414dab0d9118d4
Mahdi Khajavi <khajavim497@gmail.com>
2025-01-06 20:53:46 +0330

    Add real-time message handling in App component
    - Listen for connection events (connect, disconnect) to manage connection state.
    - Handle "newMessage" event to update Redux store with incoming messages.
    - Ensure proper cleanup of socket event listeners.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import socket from './socket'
+import ConnectionState from "./components/ConnectionState";
+import ConnectionManager from "./components/ConnectionManager";
+import ChatForm from "./components/ChatForm";
 
+import { addMessage } from "./features/messageSlice"; // Action for handling incoming messages
 
 const App = () => {
-  return (
-    <div className='text-red-700'>App</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default App
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    const onConnect = () => {
+      setIsConnected(true);
+    };
+    const onDisconnect = () => {
+      setIsConnected(false);
+    };
+    const onNewMessage = (message: { message: string; user: { id: number; userName: string }}) => {
+      dispatch(addMessage(message)); // Add the new message (with user) to Redux
+  
+    };
+    
+  
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("newMessage", onNewMessage);
+  
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("newMessage", onNewMessage);
+    };
+  }, [dispatch]);
+  
+
+
+  return (
+    <>
+      <ConnectionState isConnected={isConnected} />
+      <ConnectionManager />
+      <ChatForm /> 
+    </>
+  );
+};
+
+export default App;
